09e5f043122059ba045280dd0d2baea0e1479908
Pedro Semensato <pedrosemensato@MacBook-Pro-de-Pedro.local>
2023-11-28 23:10:13 -0300

    Merge da dev na Main com ajuste Login

--- lib/endpoints.dart
+++ lib/endpoints.dart
@@ -1,15 +1,16 @@
 class Endpoints {
-  String enviaCSV = "http://localhost:8081/remoto";
-  String recebeComunicadosPaginacao(int page, int size) {
-    return "http://localhost:8081/remoto/todos?page=$page&size=$size";
-  }
-
-  String recebeUmComunicado = "http://localhost:8081/remoto/token";
-
-  //   String enviaCSV = "https://aliancajuridico.rj.r.appspot.com/remoto";
+  // String enviaCSV = "http://localhost:8081/remoto";
   // String recebeComunicadosPaginacao(int page, int size) {
-  //   return "https://aliancajuridico.rj.r.appspot.com/remoto/todos?page=$page&size=$size";
+  //   return "http://localhost:8081/remoto/todos?page=$page&size=$size";
   // }
 
-  // String recebeUmComunicado = "https://aliancajuridico.rj.r.appspot.com/remoto/token";
+  // String recebeUmComunicado = "http://localhost:8081/remoto/token";
+
+  String enviaCSV = "https://aliancajuridico.rj.r.appspot.com/remoto";
+  String recebeComunicadosPaginacao(int page, int size) {
+    return "https://aliancajuridico.rj.r.appspot.com/remoto/todos?page=$page&size=$size";
+  }
+
+  String recebeUmComunicado =
+      "https://aliancajuridico.rj.r.appspot.com/remoto/token";
 }
