7803c7d00b3d0bb7936052e848e3653875720530
Pedro Semensato <pedrosemensato@MacBook-Pro-de-Pedro-2.local>
2024-06-03 14:23:20 -0300

    Alterado endpoints em dev para testar prod

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/pedrosemensato/Development/projetos/alianca/ruggerirosa/ruggerifrontend/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
+  /Users/pedrosemensato/Development/projetos/alianca/ruggerirosa/ruggerifrontend/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- lib/endpoints.dart
+++ lib/endpoints.dart
@@ -8,6 +8,15 @@ class Endpoints {
   String autenticaLogin = "http://localhost:8081/auth/authenticate";
   String validaToken = "http://localhost:8081/validateToken";
 
+  // String enviaCSV = "http://34.132.194.110:80/remoto";
+  // String recebeComunicadosPaginacao(int page, int size) {
+  //   return "http://34.132.194.110:80/remoto/todos?page=$page&size=$size";
+  // }
+
+  // String recebeUmComunicado = "http://34.132.194.110:80/remoto/token";
+  // String autenticaLogin = "http://34.132.194.110:80/auth/authenticate";
+  // String validaToken = "http://34.132.194.110:80/validateToken";
+
   // String enviaCSV = "https://aliancajuridico.rj.r.appspot.com/remoto";
   // String recebeComunicadosPaginacao(int page, int size) {
   //   return "https://aliancajuridico.rj.r.appspot.com/remoto/todos?page=$page&size=$size";
